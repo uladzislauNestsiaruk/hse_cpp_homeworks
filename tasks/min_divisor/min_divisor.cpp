59459f3545e90bbb51bc72f9eaeb32f59d6359f9
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-13 13:22:11 +0300

    code style fixed

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -1,8 +1,10 @@
 #include "min_divisor.h"
 
 int64_t MinDivisor(int64_t number) {
-    for (int64_t divisor = 2; divisor * divisor <= number; divisor++)
-        if (number % divisor == 0)
+    for (int64_t divisor = 2; divisor * divisor <= number; divisor++) {
+        if (number % divisor == 0) {
             return divisor;
+        }
+    }
     return number;
 }
