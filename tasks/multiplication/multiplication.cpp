3744d63f3d96e24a040ee7e958ec3bc257cfa47d
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-13 11:24:02 +0300

    multiple variables added

--- tasks/multiplication/multiplication.cpp
+++ tasks/multiplication/multiplication.cpp
@@ -1,6 +1,7 @@
 #include "multiplication.h"
 
 int64_t Multiply(int a, int b) {
-    int64_t answer = (int64_t)a * (int64_t)b;
-    return answer;
+    int64_t big_a = a;
+    int64_t big_b = b;
+    return big_a * big_b;
 }
