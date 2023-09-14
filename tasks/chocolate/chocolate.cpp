c013128faa3da3110b1452e86803e6cf47769cda
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-14 16:49:14 +0300

    chocolate code style fixed

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,16 +1,13 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    if(height * width == slices_amount){
+    if (height * width == slices_amount) {
         return false;
-    }
-    else if(slices_amount % height == 0 && slices_amount / height <= width){
+    } else if (slices_amount % height == 0 && slices_amount / height <= width) {
         return true;
-    }
-    else if(slices_amount % width == 0 && slices_amount / width <= height){
+    } else if (slices_amount % width == 0 && slices_amount / width <= height) {
         return true;
-    }
-    else{
+    } else {
         return false;
     }
 };
