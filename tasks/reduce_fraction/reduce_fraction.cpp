2b2082213280f4cc37aada2bd1d9ad8d3a598b7a
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-14 17:08:02 +0300

    reduce fraction solution added

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,6 +1,16 @@
 #include "reduce_fraction.h"
-
-
+#include <iostream>
+using std::swap;
+int64_t Gcd(int64_t a, int64_t b) {
+    if (a > b) {
+        swap(a, b);
+    }
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    return 1;
+    return Gcd(numerator, denominator);
 }
