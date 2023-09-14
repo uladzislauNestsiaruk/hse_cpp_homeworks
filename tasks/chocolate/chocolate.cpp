42e871a09fe2e11085f9b5599fc41c4973189714
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-14 16:45:16 +0300

    chocolate solution added

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,5 +1,16 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    return true;
+    if(height * width == slices_amount){
+        return false;
+    }
+    else if(slices_amount % height == 0 && slices_amount / height <= width){
+        return true;
+    }
+    else if(slices_amount % width == 0 && slices_amount / width <= height){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
