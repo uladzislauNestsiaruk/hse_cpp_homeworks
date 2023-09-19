22e5ee84421925a59c7518cc633da6dc5f097d9d
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-19 15:34:27 +0300

    code style fixed

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,17 +1,5 @@
 #include "reduce_fraction.h"
 #include <iostream>
-using std::swap;
-int64_t Gcd(int64_t a, int64_t b) {
-    if (a > b) {
-        swap(a, b);
-    }
-    while (b) {
-        a %= b;
-        swap(a, b);
-    }
-    return a;
-}
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    int64_t gcd = Gcd(numerator, denominator);
-    return (numerator + denominator) / gcd;
+    return (numerator + denominator) / std::__gcd(numerator, denominator);
 }
