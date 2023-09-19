377633e77523d52a0356539b3e7bef3a8de57a0a
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-19 13:04:03 +0300

    code style fixed

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -3,11 +3,9 @@
 bool CanBreakSlices(int height, int width, int slices_amount) {
     if (height * width == slices_amount) {
         return false;
-    } else if (slices_amount % height == 0 && slices_amount / height <= width) {
+    } else if ((slices_amount % height == 0 && slices_amount / height <= width) ||
+               (slices_amount % width == 0 && slices_amount / width <= height)) {
         return true;
-    } else if (slices_amount % width == 0 && slices_amount / width <= height) {
-        return true;
-    } else {
-        return false;
     }
+    return false;
 };
