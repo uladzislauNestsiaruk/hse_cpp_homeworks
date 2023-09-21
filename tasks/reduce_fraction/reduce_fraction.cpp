000e664d327e889706045272bff953a686cec633
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-21 14:57:14 +0300

    code style fixed

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,5 +1,7 @@
 #include "reduce_fraction.h"
+
 #include <algorithm>
+
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
     return (numerator + denominator) / std::__gcd(numerator, denominator);
 }
