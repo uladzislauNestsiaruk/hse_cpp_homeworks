0e7f6957b16ce270cfba187d1aa4dcc5b8e7f6ac
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-22 18:32:41 +0300

    fibonacci solution added

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,5 +1,11 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    return 0;
+    int64_t fibonacci_previous = 0;
+    int64_t fibonacci_current = 1;
+    for (int64_t i = 2; i <= n + 1; ++i) {
+        fibonacci_current += fibonacci_previous;
+        fibonacci_previous = fibonacci_current - fibonacci_previous;
+    }
+    return fibonacci_previous;
 }
