9d6cb813f6e6dbcfa5accc5a8cdab057d18d25c5
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-25 22:15:35 +0300

    password solution modified

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,36 +1,29 @@
 #include "password.h"
 
+constexpr size_t MinPasswordLength = 8;
+constexpr size_t MaxPasswordLength = 14;
+constexpr int8_t MinPasswordCharacterCode = 33;
+constexpr int8_t MaxPasswordCharacterCode = 126;
+
 bool ValidatePassword(const std::string& password) {
-    const size_t min_password_length = 8;
-    const size_t max_password_length = 14;
-    const int8_t min_password_character_code = 33;
-    const int8_t max_password_character_code = 126;
-    const int8_t a_ascii = 97;
-    const int8_t z_ascii = 122;
-    const int8_t capital_a_ascii = 65;
-    const int8_t capital_z_ascii = 90;
-    const int8_t zero_ascii = 48;
-    const int8_t nine_ascii = 57;
-    if (password.size() < min_password_length || password.size() > max_password_length) {
-        return false;
-    }
     int8_t lower_case_letters = 0;
     int8_t capital_letters = 0;
     int8_t digits = 0;
     int8_t other_characters = 0;
     for (size_t i = 0; i < password.size(); i++) {
-        if (password[i] < min_password_character_code || password[i] > max_password_character_code) {
+        if (password[i] < MinPasswordCharacterCode || password[i] > MaxPasswordCharacterCode) {
             return false;
         }
-        if (password[i] >= a_ascii && password[i] <= z_ascii) {
+        if (password[i] >= 'a' && password[i] <= 'z') {
             lower_case_letters = 1;
-        } else if (password[i] >= capital_a_ascii && password[i] <= capital_z_ascii) {
+        } else if (password[i] >= 'A' && password[i] <= 'Z') {
             capital_letters = 1;
-        } else if (password[i] >= zero_ascii && password[i] <= nine_ascii) {
+        } else if (password[i] >= '0' && password[i] <= '9') {
             digits = 1;
         } else {
             other_characters = 1;
         }
     }
-    return (lower_case_letters + capital_letters + digits + other_characters) > 2;
+    return ((lower_case_letters + capital_letters + digits + other_characters) > 2) &&
+           password.size() >= MinPasswordLength && password.size() <= MaxPasswordLength;
 }
