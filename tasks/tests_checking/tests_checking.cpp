a192f2518e08a817e95413ac92ce9c2675479dfa
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-09-28 17:45:12 +0300

    tests checking solution added

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -2,5 +2,20 @@
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::vector<std::string> works_order;
+    std::vector<std::string> queries_order;
+    for (auto student_action = student_actions.begin(); student_action != student_actions.end(); ++student_action) {
+        if ((*student_action).side == Side::Bottom) {
+            works_order.push_back((*student_action).name);
+        }
+    }
+    for (auto student_action : student_actions) {
+        if (student_action.side == Side::Top) {
+            works_order.push_back(student_action.name);
+        }
+    }
+    for (size_t index : queries) {
+        queries_order.push_back(works_order[works_order.size() - index]);
+    }
+    return queries_order;
 }
