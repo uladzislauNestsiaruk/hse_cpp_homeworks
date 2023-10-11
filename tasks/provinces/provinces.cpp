05085fe3ff567a119eaae50c3fb883a589d005a0
Uladzislau Nestsiaruk <vldnstrk@gmail.com>
2023-10-11 11:24:40 +0300

    code style fixed

--- tasks/provinces/provinces.cpp
+++ tasks/provinces/provinces.cpp
@@ -3,7 +3,7 @@
 int64_t CountPassports(const std::vector<int>& provinces) {
     int64_t new_passports_amount = 0;
     std::multiset<int64_t> provinces_size_order(provinces.begin(), provinces.end());
-    while(provinces_size_order.size() > 1){
+    while (provinces_size_order.size() > 1) {
         new_passports_amount += *provinces_size_order.begin() + *(++provinces_size_order.begin());
         provinces_size_order.insert(*provinces_size_order.begin() + *(++provinces_size_order.begin()));
         provinces_size_order.erase(provinces_size_order.begin());
